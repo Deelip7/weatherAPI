17678ac8fdeb1619d5017152698e8db986386c70
Theelipan Prabakar <theelipan@gmail.com>
2020-04-29 15:34:04 -0400

    fetch api

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
+// User Inputs
+var input = document.querySelector(".weather-form__input");
+var button = document.querySelector(".weather-form__button");
+
+//API Values
+var locationName = document.querySelector(".weather-output__location");
+var description = document.querySelector(".weather-output__descriptions");
+var temp = document.querySelector(".weather-output__temp");
+var feelsLike = document.querySelector(".weather-output__feelslike");
+var humidity = document.querySelector(".weather-output__humidity");
+var wind = document.querySelector(".weather-output__wind");
+
 fetch(
   "https://api.openweathermap.org/data/2.5/weather?zip=" +
     "11801" +
     "&appid=1e12d953ad0dc8ff3a4f488ae456a976"
 )
   .then((res) => res.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+
+    var 
+
+
+
+
+
+  });
