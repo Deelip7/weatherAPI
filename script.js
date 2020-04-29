79bd602f813fc3a735483500c4d002094c75e535
Theelipan Prabakar <theelipan@gmail.com>
2020-04-29 16:42:48 -0400

    display data in HTML elements

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ var button = document.querySelector(".weather-form__button");
 
 //API Values
 var locationName = document.querySelector(".weather-output__location");
-var description = document.querySelector(".weather-output__descriptions");
+var descriptions = document.querySelector(".weather-output__descriptions");
 var temp = document.querySelector(".weather-output__temp");
 var feelsLike = document.querySelector(".weather-output__feelslike");
 var humidity = document.querySelector(".weather-output__humidity");
 var wind = document.querySelector(".weather-output__wind");
+var icon = document.querySelector(".weather-output__icon");
 
 fetch(
   "https://api.openweathermap.org/data/2.5/weather?zip=" +
@@ -17,25 +18,23 @@ fetch(
 )
   .then((res) => res.json())
   .then((data) => {
-    var locationName = data["name"];
+    var location = data["name"];
     // var tempCel = Math.round(data.main["temp"] - 273.15);
     // var tempFah = Math.round(((data.main["temp"] - 273.15) * 9) / 5 + 32);
-    var tempCel = data.main["temp"];
-    var tempFah = data.main["temp"];
-    var description = data.weather[0]["main"];
+    var tempK = data.main["temp"];
+    var description1 = data.weather[0]["main"];
     var description2 = data.weather[0]["description"];
-    var feelsLike = data.main["feels_like"];
-    var humidity = data.main["humidity"] + "%";
-    var wind = data.wind["speed"] + "mph";
+    var feels_Like = data.main["feels_like"];
+    var humid = data.main["humidity"] + "%";
+    var windspeed = data.wind["speed"] + "mph";
+    var iconCode = data.weather[0].icon;
+    var iconUrl = "http://openweathermap.org/img/wn/" + iconCode + ".png";
 
-    console.log(
-      locationName,
-      tempCel,
-      tempFah,
-      description,
-      description2,
-      feelsLike,
-      humidity,
-      wind
-    );
+    temp.innerHTML = tempK;
+    locationName.innerHTML = location;
+    descriptions.innerHTML = description2;
+    humidity.innerHTML = humid;
+    wind.innerHTML = windspeed;
+    feelsLike.innerHTML = feels_Like;
+    icon.innerHTML = "<img src='" + iconUrl + "'>";
   });
